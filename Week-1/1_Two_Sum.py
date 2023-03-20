21ef83bab2bd328ab694e9cdbd0c1b00c4fef230
David Portilla <97.david.pm@gmail.com>
2023-03-19 20:44:53 -0600

    Update 1_Two_Sum.py

--- Week-1/1_Two_Sum.py
+++ Week-1/1_Two_Sum.py
@@ -1,7 +1,7 @@
 def twoSum(nums, target):
-        my_hash = {num: index for index, num in enumerate(nums)}
-        actual_index = 0
-        for item in nums:
-            if ((target - item) in my_hash and my_hash[target - item] is not actual_index):
-                return [my_hash[target - item], actual_index]
-            actual_index += 1
+    my_hash = {num: index for index, num in enumerate(nums)}
+    actual_index = 0
+    for item in nums:
+        if ((target - item) in my_hash and my_hash[target - item] is not actual_index):
+            return [my_hash[target - item], actual_index]
+        actual_index += 1
